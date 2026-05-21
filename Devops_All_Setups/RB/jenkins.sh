044f4143242dda8f1433e711d6091693c52ed2c1
Dasari Raviteja <dasariraviteja7172@gmail.com>
2026-05-21 19:16:52 +0530

    Update jenkins.sh

--- Devops_All_Setups/RB/jenkins.sh
+++ Devops_All_Setups/RB/jenkins.sh
@@ -1,11 +1,12 @@
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
-sudo yum install jenkins git -y
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
+sudo yum upgrade -y
+sudo yum install java-21-amazon-corretto-devel git -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
 sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo systemctl start Jenkins
 sudo systemctl status jenkins
 sudo mkdir -p /var/tmp_disk
 sudo chmod 1777 /var/tmp_disk
